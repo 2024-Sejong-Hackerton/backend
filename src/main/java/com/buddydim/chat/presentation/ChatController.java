04ccdcaf58696b84a6877f2cd8a825168688c9fe
kokodak <kokodakadokok@gmail.com>
2024-12-27 01:13:14 +0900

    Implements async logic

--- src/main/java/com/buddydim/chat/presentation/ChatController.java
+++ src/main/java/com/buddydim/chat/presentation/ChatController.java
@@ -2,31 +2,35 @@ package com.buddydim.chat.presentation;
 
 import com.buddydim.chat.application.ChatService;
 import com.buddydim.chat.presentation.dto.PostChatRequest;
-import java.util.List;
+import com.buddydim.jwt.JwtUtil;
+import java.net.URI;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
-    private final ChatService chatService;
 
-    public ChatController(ChatService chatService) {
+    private final ChatService chatService;
+    private final JwtUtil jwtUtil;
+
+    public ChatController(final ChatService chatService,
+                          final JwtUtil jwtUtil) {
         this.chatService = chatService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/chat")
-    public ResponseEntity<Void> postChat(@RequestBody PostChatRequest request) {
-        chatService.saveContent(request);
-        return ResponseEntity.ok().build();
-    }
-
-    // 채팅 기록 조회
-    @GetMapping("/history")
-    public ResponseEntity<List<String>> getChatHistory() {
-        List<String> chatHistory = chatService.getChatHistory();
-        return ResponseEntity.ok(chatHistory);
+    public ResponseEntity<Void> createChat(@RequestBody PostChatRequest request,
+                                           @RequestHeader("Authorization") String authHeader) {
+        String token = authHeader.replace("Bearer ", "");
+        Long userId = jwtUtil.getUserIdFromToken(token);
+        final Long chatId = chatService.createChat(request, userId);
+        return ResponseEntity.status(HttpStatus.OK)
+                             .location(URI.create("/chat/status/" + chatId))
+                             .build();
     }
 }
